21f235e28377dfd9656c3c77b46926d32b9b29fe
Pgwsunny <814424014@qq.com>
2019-08-22 23:22:25 +0800

    practice03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,16 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name,age);
+    }
+
+    public Worker() {
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
+
