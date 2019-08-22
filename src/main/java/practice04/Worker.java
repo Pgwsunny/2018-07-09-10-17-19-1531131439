15018ad542a7c03001c753673689ca1b110a7202
Pgwsunny <814424014@qq.com>
2019-08-22 23:40:37 +0800

    practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Worker {
+import practice03.Person;
+
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name,age);
+    }
+
+    public Worker() {
+    }
+
+    @Override
+    public String introduce() {
+    	String str = super.introduce();
+        return str + " I am a Worker. I have a job.";
+    }
 }
